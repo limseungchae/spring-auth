5b18e6430e9fa5e012af11a83d72491b1ed5de4c
admin <tmdco12@naver.com>
2023-11-04 12:23:41 +0900

    controller - 쿠키 다루기

---
+++ src/main/java/com/example/springauth/controller/AuthController.java
@@ -0,0 +1,52 @@
+package com.example.springauth.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    // 쿠키 생성
+    @GetMapping("/create-cookie")
+    public String createCookie(HttpServletResponse res) {
+        addCookie("Robbie Auth", res);
+
+        return "createCookie";
+    }
+
+    // 쿠기 읽기
+    @GetMapping("/get-cookie")
+    public String getCookie(@CookieValue(AUTHORIZATION_HEADER) String value) {
+        System.out.println("value = " + value);
+
+        return "getCookie : " + value;
+    }
+
+    // 쿠키 생성
+    public static void addCookie(String cookieValue, HttpServletResponse res) {
+        try {
+            cookieValue = URLEncoder.encode(cookieValue, "utf-8").replaceAll("\\+", "%20");
+            // Cookie Value 에는 공백이 불가능해서 encoding 진행
+
+            Cookie cookie = new Cookie(AUTHORIZATION_HEADER, cookieValue); // Name-Value
+            cookie.setPath("/"); // 쿠키 범위
+            cookie.setMaxAge(30 * 60); // 만료시간 : 30분
+
+            // Response 객체에 Cookie 추가
+            // 응답 객체에 쿠키 추가
+            res.addCookie(cookie);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
