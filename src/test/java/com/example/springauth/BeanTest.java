8d087a3cafb20ca5fdf0d10b079972ed268b4815
admin <tmdco12@naver.com>
2023-11-04 09:47:32 +0900

    @Qualifier, @Primary 동시 적용시 @Qualifier 우선수위

--- src/test/java/com/example/springauth/BeanTest.java
+++ src/test/java/com/example/springauth/BeanTest.java
@@ -13,5 +13,12 @@ public class BeanTest {
     @Autowired
     @Qualifier("pizza")
     Food food;
-    
+
+    @Test
+    @DisplayName("Primary 와 Qualifier 우선순위 확인")
+    void test1() {
+        // 현재 Chicken 은 Primary 가 적용된 상태
+        // Pizza는 Qualifier 가 추가된 상태입니다.
+        food.eat();
+    }
 }
